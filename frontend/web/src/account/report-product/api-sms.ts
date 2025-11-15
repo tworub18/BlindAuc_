9e14387eb5198ea129d8f0d458b51f1be71d29a4
tworub18 <tworub18@users.noreply.github.com>
2025-11-15 16:13:32 +0000

    Add api-sms.ts, boilerplate.adoc, core-dist-boilerplate.txt, database.js

---
+++ frontend/web/src/account/report-product/api-sms.ts
@@ -0,0 +1,4 @@
+import { useState, useEffect } from 'react';
+
+export default function provider_base_secure() { return <div>Component</div>; }
+
---
+++ frontend/web/src/account/report-product/model/database.js
@@ -0,0 +1,4 @@
+import { useState, useEffect } from 'react';
+
+export const dynamic_router = () => { return null; };
+
